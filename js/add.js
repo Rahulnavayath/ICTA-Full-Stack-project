83237097eba4901f2f469f6efb4361d085da27db
Rahulnavayath <rahulans44@gmail.com>
2019-03-26 15:29:25 +0530

    greatestnumber in 3

---
+++ js/add.js
@@ -0,0 +1,4 @@
+var x=250;
+var y=2250;
+var z=x+y;
+console.log(z);
---
+++ js/greatestnum.js
@@ -0,0 +1,16 @@
+var a=150;
+var b=250;
+var c=300;
+
+if(a>b)
+
+{if(a>c)
+    console.log(a);
+}
+
+else if (b>c){
+    console.log(b);
+}
+else{
+    console.log(c);
+}
