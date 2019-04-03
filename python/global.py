267b09af64c7f3698d9974bc3baf07b8d355f635
Rahulnavayath <48901342+Rahulnavayath@users.noreply.github.com>
2019-04-03 10:22:02 +0530

    global and local variable

---
+++ python/global.py
@@ -0,0 +1,5 @@
+def test():
+	print "inside test x is" ,x
+x=50
+test()
+print "value of x is" ,x
---
+++ python/local.py
@@ -0,0 +1,7 @@
+def test():
+	global y
+	y=20
+	print "value of x is" ,x,"y is" , y
+x=50
+test()
+print "value of x is" ,x, "y is" ,y
