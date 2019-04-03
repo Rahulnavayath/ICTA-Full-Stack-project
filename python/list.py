5f3bbb8934d8cb3dfee75fea904eb787c175f643
Rahulnavayath <48901342+Rahulnavayath@users.noreply.github.com>
2019-04-03 15:13:22 +0530

    list methods insert remove

---
+++ python/list.py
@@ -0,0 +1,30 @@
+#a=[1,"hello",3.5,[2,"welcome",4.5]]
+#print a[1]
+#print a[2]
+#print a[3][1]
+#for i in a:
+	#print i
+
+#a=[1,2,3]
+#b=a+[1]
+#print b
+#c=b*3
+#print c
+
+a=[1,2,3]
+a.append(4)	#to add an element in last
+print a
+
+b=[5,6,7]
+a.extend(b)	#to add second list to the first
+print a
+
+a=[1,3,4,5]
+a.insert(1,2)	#to insert an element
+print a
+
+a=[1,2,3]
+a.pop(0)	#to delete an element
+print a
+
+
