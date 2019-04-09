ec132cd02903eb2642ff36c6e98fb6fbdaebb7a2
Rahulnavayath <48901342+Rahulnavayath@users.noreply.github.com>
2019-04-09 16:21:21 +0530

    application creation

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,12 +1,43 @@
 const express=require('express');
+const exphbs=require('express-handlebars')
+
 var app=express();
-app.get('/', (req,res)=>{
-    res.send("WELCOME TO OOTTY");
+app.engine('handlebars',exphbs({defaultLayout:'mainpage'}));
+app.set('view engine','handlebars');
+app.use(express.static('views/static'));
+app.use(express.urlencoded());
+
+app.get('/',(req,res)=>{
+    res.render('index');
 });
-app.get('/home',(req,res)=>{
-    res.send("Welcome To Home");
-})
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
 app.get('/contact',(req,res)=>{
-    res.send("Contact Page");
-})
-app.listen(3000);
+    res.render('contact');
+});
+app.get('/gallery',(req,res)=>{
+    res.render('gallery');
+});
+app.post('/getdata',(req,res)=>{
+   // res.send('DATA ACCEPTED');
+   var name=req.body.name;
+   var address=req.body.address;
+   var place=req.body.place;
+   var email=req.body.email;
+   var mobile=req.body.mob;
+   
+   console.log(name,address,place,email,mobile);
+});
+
+//var app=express();
+//app.get('/2', (req,res)=>{
+    //res.send("<marquee><h1> WELCOME TO OOTTY</h1></marquee>");
+//});
+//app.get('/home',(req,res)=>{
+   // res.send("Welcome To Home");
+//})
+//app.get('/contact',(req,res)=>{
+   // res.send('[{"name": "Tom","age":23},{"name":"Riya","age":23}]');
+//})
+app.listen(3030);
